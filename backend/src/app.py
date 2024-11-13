caa18864ebf8b1a50ef37bc2767f6c4b349e2cdf
lealre <renaanvp@gmail.com>
2024-11-13 16:12:34 +0000

    Implement raw JWT authentication for login

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.routers import auth, author, books, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,6 +10,15 @@ app.include_router(auth.router)
 app.include_router(books.router)
 app.include_router(author.router)
 
+origins = ['http://localhost:5173']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get('/')
 def home_root():
