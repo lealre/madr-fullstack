e49cc88fc06f1ab8b419bd5150d2a925e8828e50
lealre <renaanvp@gmail.com>
2024-11-14 21:53:42 +0000

    front: Working in dashboard layout

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,12 +1,70 @@
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
+import api from "../api";
 
-const Dashboard = () => {
+interface AuthorsProps {
+  id: number;
+  author: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [authors, setAuthors] = useState<AuthorsProps[]>([]);
+
+  const fetchAuthors = async (): Promise<void> => {
+    try {
+      const response = await api.get("/author/");
+      setAuthors(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAuthors();
+  }, []);
   return (
-    <div>
-      <Header></Header>
-      <h1>Welcome to the Dashboard!</h1>
-      <p>You are logged in and can access this page.</p>
-    </div>
+    <>
+        <Header />
+
+      <div className="container text-center mt-5">
+        <table
+          className="table"
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            backgroundColor: "#bcb8b1",
+            color: "#463f3a",
+          }}
+        >
+          <thead style={{ backgroundColor: "#e0afa0" }}>
+            <tr>
+              <th style={{ padding: "10px", border: "1px solid #8a817c" }}>
+                Id
+              </th>
+              <th style={{ padding: "10px", border: "1px solid #8a817c" }}>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {authors.length > 0 ? (
+              authors.map((item) => (
+                <tr key={item.id}>
+                  <td style={{ padding: "10px", border: "1px solid #8a817c" }}>
+                    {item.id}
+                  </td>
+                  <td style={{ padding: "10px", border: "1px solid #8a817c" }}>
+                    {item.author}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <p>No data found</p>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import {useLocation, useNavigate } from "react-router-dom";
 
 
 import Header from "../components/Header";
@@ -30,21 +30,8 @@ const HomePage: React.FC = () => {
       <div className="container text-center mt-5">
         <h1 style={{ color: "#463f3a" }}>Welcome to MADR App!</h1>
         <p style={{ color: "#8a817c" }}>
-          This is an introduction to the app. Please login to continue to your
-          dashboard.
+          This is an introduction to the app.
         </p>
-        <Link
-          to="/login"
-          className="btn"
-          style={{
-            backgroundColor: "#e0afa0",
-            color: "#463f3a",
-            borderColor: "#bcb8b1",
-            textDecoration: "none",
-          }}
-        >
-          Login
-        </Link>
         {message && (
           <div
             className="alert-modal"
