296855c9f58a346a888b8f50165a3ce22fd557d3
lealre <renaanvp@gmail.com>
2024-12-27 22:56:14 +0000

    tests: All tests passing and `mypy` checks successful

--- backend/tests/routes/test_book.py
+++ backend/tests/routes/test_book.py
@@ -1,9 +1,15 @@
 from http import HTTPStatus
 
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models import Author, Book
 from tests.conftest import BookFactory
 
 
-async def test_add_book(async_client, user_token, author):
+async def test_add_book(
+    async_client: AsyncClient, user_token: str, author: Author
+) -> None:
     response = await async_client.post(
         '/book',
         headers={'Authorization': f'Bearer {user_token}'},
@@ -19,7 +25,9 @@ async def test_add_book(async_client, user_token, author):
     }
 
 
-async def test_add_book_already_exists(async_client, user_token, book):
+async def test_add_book_already_exists(
+    async_client: AsyncClient, user_token: str, book: Book
+) -> None:
     response = await async_client.post(
         '/book',
         headers={'Authorization': f'Bearer {user_token}'},
@@ -30,7 +38,9 @@ async def test_add_book_already_exists(async_client, user_token, book):
     assert response.json() == {'detail': f'{book.title} already in MADR.'}
 
 
-async def test_add_book_author_id_not_found(async_client, user_token):
+async def test_add_book_author_id_not_found(
+    async_client: AsyncClient, user_token: str
+) -> None:
     book = BookFactory()
     response = await async_client.post(
         '/book',
@@ -48,7 +58,7 @@ async def test_add_book_author_id_not_found(async_client, user_token):
     }
 
 
-async def test_add_book_not_authenticated(async_client):
+async def test_add_book_not_authenticated(async_client: AsyncClient) -> None:
     response = await async_client.post(
         '/book',
         json={'year': 2024, 'title': 'book title', 'author_id': 1},
@@ -59,8 +69,8 @@ async def test_add_book_not_authenticated(async_client):
 
 
 async def test_book_title_sanitization_schema(
-    async_client, user_token, author
-):
+    async_client: AsyncClient, user_token: str, author: Author
+) -> None:
     expected_title = 'a title to correct'
     response = await async_client.post(
         '/book',
@@ -75,7 +85,9 @@ async def test_book_title_sanitization_schema(
     assert response.json()['title'] == expected_title
 
 
-async def test_delete_book(async_client, user_token, book):
+async def test_delete_book(
+    async_client: AsyncClient, user_token: str, book: Book
+) -> None:
     response = await async_client.delete(
         f'/book/{book.id}', headers={'Authorization': f'Bearer {user_token}'}
     )
@@ -84,7 +96,9 @@ async def test_delete_book(async_client, user_token, book):
     assert response.json() == {'message': 'Book deleted from MADR.'}
 
 
-async def test_delete_book_not_found(async_client, user_token, book):
+async def test_delete_book_not_found(
+    async_client: AsyncClient, user_token: str, book: Book
+) -> None:
     response = await async_client.delete(
         f'/book/{10}', headers={'Authorization': f'Bearer {user_token}'}
     )
@@ -93,14 +107,21 @@ async def test_delete_book_not_found(async_client, user_token, book):
     assert response.json() == {'detail': 'Book not found in MADR.'}
 
 
-async def test_delete_book_not_authenticated(async_client, book):
+async def test_delete_book_not_authenticated(
+    async_client: AsyncClient, book: Book
+) -> None:
     response = await async_client.delete(f'/book/{book.id}')
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Not authenticated'}
 
 
-async def test_patch_book(async_session, async_client, user_token, author):
+async def test_patch_book(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+    user_token: str,
+    author: Author,
+) -> None:
     input_year = 2000
     book = BookFactory(year=input_year)
 
@@ -126,7 +147,9 @@ async def test_patch_book(async_session, async_client, user_token, author):
     }
 
 
-async def test_patch_book_not_found(async_client, user_token, book):
+async def test_patch_book_not_found(
+    async_client: AsyncClient, user_token: str, book: Book
+) -> None:
     response = await async_client.patch(
         f'/book/{10}',
         headers={'Authorization': f'Bearer {user_token}'},
@@ -137,14 +160,16 @@ async def test_patch_book_not_found(async_client, user_token, book):
     assert response.json() == {'detail': 'Book not found in MADR.'}
 
 
-async def test_patch_book_not_authenticated(async_client, book):
+async def test_patch_book_not_authenticated(
+    async_client: AsyncClient, book: Book
+) -> None:
     response = await async_client.patch(f'/book/{10}', json={'year': 2000})
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Not authenticated'}
 
 
-async def test_get_book_by_id(async_client, book):
+async def test_get_book_by_id(async_client: AsyncClient, book: Book) -> None:
     response = await async_client.get(f'/book/{book.id}')
 
     assert response.status_code == HTTPStatus.OK
@@ -156,22 +181,24 @@ async def test_get_book_by_id(async_client, book):
     }
 
 
-async def test_get_book_by_id_not_found(async_client, book):
+async def test_get_book_by_id_not_found(
+    async_client: AsyncClient, book: Book
+) -> None:
     response = await async_client.get(f'/book/{10}')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Book not found in MADR.'}
 
 
-async def test_list_books_empty(async_client):
+async def test_list_books_empty(async_client: AsyncClient) -> None:
     response = await async_client.get('/book')
 
     assert response.json()['books'] == []
 
 
 async def test_list_books_filter_name_should_return_5_books(
-    async_client, async_session, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     expected_books = 5
     async_session.add_all(BookFactory.create_batch(5))
     books_with_title = BookFactory.create_batch(5, title='title')
@@ -180,25 +207,25 @@ async def test_list_books_filter_name_should_return_5_books(
     async_session.add_all(books_with_title)
     await async_session.commit()
 
-    response = await async_client.get('/book/?name=oo')
+    response = await async_client.get('/book/?title=title')
 
-    assert len(response.json()['books']) == expected_books
+    assert response.json()['total_results'] == expected_books
 
 
 async def test_list_books_filter_name_should_return_empty(
-    async_client, async_session, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     async_session.add_all(BookFactory.create_batch(5))
     await async_session.commit()
 
-    response = await async_client.get('/book/?name=title')
+    response = await async_client.get('/book/?title=title')
 
     assert response.json()['books'] == []
 
 
 async def test_list_books_filter_year_should_return_5_books(
-    async_client, async_session, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     expected_books = 5
     async_session.add_all(BookFactory.create_batch(5, year=2000))
     async_session.add_all(BookFactory.create_batch(5, year=2024))
@@ -210,8 +237,8 @@ async def test_list_books_filter_year_should_return_5_books(
 
 
 async def test_list_books_filter_year_should_return_empty(
-    async_client, async_session, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     async_session.add_all(BookFactory.create_batch(5, year=2000))
     await async_session.commit()
 
@@ -221,8 +248,8 @@ async def test_list_books_filter_year_should_return_empty(
 
 
 async def test_list_books_filter_combined_should_return_5_books(
-    async_session, async_client, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     expected_books = 5
     books = BookFactory.create_batch(7, year=2000)
     books[-1].title = 'title'
@@ -230,18 +257,18 @@ async def test_list_books_filter_combined_should_return_5_books(
     async_session.add_all(books)
     await async_session.commit()
 
-    response = await async_client.get('/book/?year=2000&name=oo')
+    response = await async_client.get('/book?year=2000&title=oo')
 
     assert len(response.json()['books']) == expected_books
 
 
 async def test_list_books_pagination_should_return_20_books(
-    async_session, async_client, author
-):
+    async_client: AsyncClient, async_session: AsyncSession, author: Author
+) -> None:
     expected_books = 20
     async_session.add_all(BookFactory.create_batch(25, year=2000))
     await async_session.commit()
 
-    response = await async_client.get('/book/?title=2000')
+    response = await async_client.get('/book?year=2000')
 
     assert len(response.json()['books']) == expected_books
